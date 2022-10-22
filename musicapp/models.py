0b4fc7a39aa63236ce854479080ac479628116fe
Mr Haywhy <tosinblessing.akinyemi@gmail.com>
2022-10-22 10:48:47 +0100

    update

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
-from django.http import HttpResponse
 
 
 # Create your models here.
-def index(request):
-    return HttpResponse("Yayyyyy")
+class Artise(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.CharField(max_length=200)
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=500)
+    sond_id = models.CharField(max_length=200)
